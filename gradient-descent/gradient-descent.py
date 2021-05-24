db6b43bbb8f681f2a803f823dd05fb97aedebbbb
Chris Grounds <chrisbacon2009@hotmail.com>
2021-05-24 14:30:21 +0100

    0 index theta variables

--- gradient-descent/gradient-descent.py
+++ gradient-descent/gradient-descent.py
@@ -2,47 +2,47 @@ epochs = 200
 learning_rate = 0.1
 
 
-def mean_squared_loss(hypothesis, xs, ys, t1, t2):
+def mean_squared_loss(hypothesis, xs, ys, t0, t1):
     sum_delta_squared = 0
 
     for i, _ in enumerate(xs):
-        sum_delta_squared += (hypothesis(t1, t2, xs[i]) - ys[i]) ** 2
+        sum_delta_squared += (hypothesis(t0, t1, xs[i]) - ys[i]) ** 2
 
     return (1 / (2 * len(xs))) * sum_delta_squared
 
 
 def gradient_descent(hypothesis, xs, ys):
-    theta1, theta2 = 0, 1
+    theta0, theta1 = 0, 1
 
     e = 0
     while e < epochs:
         e += 1
+        sum_differences_t0 = 0
         sum_differences_t1 = 0
-        sum_differences_t2 = 0
 
         for i, _ in enumerate(xs):
-            sum_differences_t1 += hypothesis(theta1, theta2, xs[i]) - ys[i]
+            sum_differences_t0 += hypothesis(theta0, theta1, xs[i]) - ys[i]
 
         for i, _ in enumerate(xs):
-            sum_differences_t2 += (hypothesis(theta1, theta2, xs[i]) - ys[i]) * xs[i]
+            sum_differences_t1 += (hypothesis(theta0, theta1, xs[i]) - ys[i]) * xs[i]
 
+        derivative_of_loss_t0 = (1 / len(xs)) * sum_differences_t0
         derivative_of_loss_t1 = (1 / len(xs)) * sum_differences_t1
-        derivative_of_loss_t2 = (1 / len(xs)) * sum_differences_t2
 
-        loss = mean_squared_loss(hypothesis, xs, ys, theta1, theta2)
+        loss = mean_squared_loss(hypothesis, xs, ys, theta0, theta1)
         print("loss: {}".format(loss))
 
+        theta0 = theta0 - learning_rate * derivative_of_loss_t0
         theta1 = theta1 - learning_rate * derivative_of_loss_t1
-        theta2 = theta2 - learning_rate * derivative_of_loss_t2
 
-    return theta1, theta2
+    return theta0, theta1
 
 
 xs = [1, 2, 3, 4]
 ys = [2, 4, 6, 8]
 
-theta1 = 0
-theta2 = 1
-def hypothesis(t1, t2, x): return t1 + t2 * x
+theta0 = 0
+theta1 = 1
+def hypothesis(t0, t1, x): return t0 + t1 * x
 
 print(gradient_descent(hypothesis, xs, ys))
