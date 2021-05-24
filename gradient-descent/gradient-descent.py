3b4c4c4d2f80b3cfca8fdab657dc8215f3a89a79
Chris Grounds <chrisbacon2009@hotmail.com>
2021-05-24 12:10:10 +0100

    first gradient descent impl

--- gradient-descent/gradient-descent.py
+++ gradient-descent/gradient-descent.py
@@ -7,25 +7,39 @@ def mean_squared_loss(hypothesis, xs, ys):
 
     i = 0
     while i < len(xs):
-        sum_delta_squared = sum_delta_squared + \
-            ((hypothesis(xs[i]) - ys[i]) ** 2)
-        i = i + 1
+        sum_delta_squared += (hypothesis(xs[i]) - ys[i]) ** 2
+        i += 1
 
     return (1 / (2 * len(xs))) * sum_delta_squared
 
 
-def gradient_descent(t1, t2, loss):
-    # derivative_of_loss_t1 = (1 / len(xs)) * (sum((hypothesis(xs[i]) - ys[i])))
-    # derivative_of_loss_t2 = (1 / len(xs)) * \
-    #     (sum((hypothesis(xs[i]) - ys[i]) * xs[i]))
+def gradient_descent(xs, ys):
+    theta1 = 0
+    theta2 = 1
+    def hypothesis(x): return theta1 + theta2 * x
 
-    derivative_of_loss_t1 = loss
-    derivative_of_loss_t2 = loss * xs[i]
+    e = 0
+    while e < epochs:
+        e += 1
+        i, j, sum_differences = 0, 0, 0
 
-    new_t1 = t1 - learning_rate * derivative_of_loss_t1
-    new_t2 = t2 - learning_rate * derivative_of_loss_t2
+        while i < len(xs):
+            sum_differences += hypothesis(xs[i]) - ys[i]
 
-    return new_t1, new_t2
+            i += 1
+
+        while j < len(xs):
+            derivative_of_loss_t1 = (1 / len(xs)) * sum_differences
+            derivative_of_loss_t2 = (1 / len(xs)) * sum_differences * xs[j]
+
+            print(sum_differences)
+
+            theta1 = theta1 - learning_rate * derivative_of_loss_t1
+            theta2 = theta2 - learning_rate * derivative_of_loss_t2
+
+            j += 1
+
+    return theta1, theta2
 
 
 xs = [3, 1, 0, 4]
@@ -36,9 +50,10 @@ theta2 = 1
 def hypothesis(x): return theta1 + theta2 * x
 
 
-i = 0
-while i < epochs:
-    i = i + 1
-    loss = mean_squared_loss(hypothesis, xs, ys)
-    # theta1, theta2 = gradient_descent(theta1, theta2, loss)
-    print("loss: {}".format(loss))
+# i = 0
+# while i < epochs:
+#     i = i + 1
+#     loss = mean_squared_loss(hypothesis, xs, ys)
+#     print("loss: {}".format(loss))
+
+print(gradient_descent(xs, ys))
